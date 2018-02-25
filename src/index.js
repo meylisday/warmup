399a26571570362f55a028706be35474410116a1
meylisday <victory.sett@gmail.com>
2018-02-25 18:32:38 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 module.exports = function warmup(temperature) {
-  var test = (temperature === -20) ? -4 : 
-  (temperature === 0) ? 32 :
-  (temperature === 15) ? 59 :
-  (temperature === 40) ? 104 : false;
-  return test;
+  var farengheit = temperature * 1.8 + 32;
+  return farengheit;
 };
-
